daba524eeaab523bd43fd2a7513e783abaa6602d
Seth Cohen <smcohen82@gmail.com>
2021-02-12 19:34:29 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,56 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  result = nil
+  collection.each do |food_item|
+    food_item.each do |k, v|
+      if food_item[k] == name
+        result = food_item
+      end
+    end
+  end
+  result
 end
+
+=begin 
+def consolidate_cart(cart)
+ consolidated_cart = []
+ cart.each do |food_item|
+  current_food_item = find_item_by_name_in_collection(food_item[:item], consolidated_cart)
+  if current_food_item
+    consolidated_cart.each do |new_food_item|
+      if new_food_item[:item] == current_food_item[:item]
+        binding.pry
+        new_food_item[:count] += 1
+      end
+    end
+  else
+    food_item[:count] = 1
+    consolidated_cart << food_item
+  end
+ end
+ consolidated_cart
+end
+=end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+ consolidated_cart = []
+ cart.each do |food_item|
+  current_food_item = find_item_by_name_in_collection(food_item[:item], consolidated_cart)
+  if current_food_item
+    consolidated_cart.each do |new_food_item|
+      if new_food_item[:item] == current_food_item[:item]
+        new_food_item[:count] += 1
+      end  
+    end
+  else
+    food_item[:count] = 1
+    consolidated_cart << food_item
+  end
+ end
+ consolidated_cart
 end
 
-
-  
+puts consolidate_cart([{:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "KALE", :price => 3.00, :clearance => false},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "AVOCADO", :price => 3.00, :clearance => true}, {:item => "BLACK_BEANS", :price => 2.50, :clearance => false}, {:item => "Melville", :price => 2.50, :clearance => false}])
